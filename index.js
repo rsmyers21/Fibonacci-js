9fa26a099856f37fe5bda5479428579f1710f06c
rsmyers21 <rsmyers21@gmail.com>
2023-11-21 16:58:02 -0600

    Completed final version of fibonacci algorithm

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-console.log("test");
+/*Program that implements a fibonacci alogorithm and returns the first n numbers in Javascript */
 
 function fibonacci (n) {
+    const fibArray = [0, 1];
     
+    for (let i=2; i < n; i++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return fibArray
 }
+
+console.log(fibonacci(10));
